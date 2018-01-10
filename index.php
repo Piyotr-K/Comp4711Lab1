3093952c04d14e05b8229b339a347e5bfcb238ff
Piyotr-K <piyotrkao1998@Outlook.com>
2018-01-10 12:01:15 -0800

    Added 2 students and made them print

--- index.php
+++ index.php
@@ -5,6 +5,30 @@
  * Date: 2018-01-10
  * Time: 11:01 AM
  */
+include('Student.php');
+
+$students = array();
+
+$first = new Student();
+$first->surname = "Doe";
+$first->first_name = "John";
+$first->add_email('home','john@doe.com');
+$first->add_email('work','jdoe@mcdonalds.com');
+$first->add_grade(65);
+$first->add_grade(75);
+$first->add_grade(55);
+$students['j123'] = $first;
+
+$second = new Student();
+$second->surname = "Einstein";
+$second->first_name = "Albert";
+$second->add_email('home','albert@braniacs.com');
+$second->add_email('work1','a_einstein@bcit.ca');
+$second->add_email('work2','albert@physics.mit.edu');
+$second->add_grade(95);
+$second->add_grade(80);
+$second->add_grade(50);
+$students['a456'] = $second;
 ?>
 <html>
 <head>
@@ -13,7 +37,8 @@
 </head>
 <body>
 <?php
-    echo "Test boyz";
+    foreach ($students as $student)
+        echo $student->toString();
 ?>
 </body>
 </html>
