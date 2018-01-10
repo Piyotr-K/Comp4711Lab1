785897c8a46b21d8393b56978125dea43e51c361
Piyotr-K <piyotrkao1998@Outlook.com>
2018-01-10 11:04:56 -0800

    Setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lel
+ * Date: 2018-01-10
+ * Time: 11:01 AM
+ */
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+<body>
+<?php
+    echo "Test boyz";
+?>
+</body>
+</html>
